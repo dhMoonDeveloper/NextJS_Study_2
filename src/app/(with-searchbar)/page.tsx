eb949383928f0c8238114eee91639ccf0accda2a
DONGHYUN_PC\xlr3z <dhmoon1023@gmail.com>
2026-02-24 17:01:28 +0900

    페이지 캐싱

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,7 +1,57 @@
-import styles from "./page.module.css";
+import BookItem from "@/components/book-item";
+import style from "./page.module.css";
+import { BookData } from "@/types";
+
+// export const dynamic = ''
+// 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
+// 1. auto : 기본값, 아무것도 강제하지 않음
+// 2. force-dynamic
+// 3. force-static
+// 4. error
+
+async function AllBooks() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, { cache : "force-cache"});
+  if(!response.ok) {
+      return <div>오류가 발생했습니다...</div>
+  }
+  const allBooks: BookData[] = await response.json();
+
+  
+  return (
+    <div>
+      {allBooks.map((book) => (
+          <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  )
+
+}
+
+async function RecoBooks() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`, { next : { revalidate : 3 }});
+  if(!response.ok) {
+      return <div>오류가 발생했습니다...</div>
+  }
+  const recoBooks: BookData[]  = await response.json();
+
+  return <div>
+    {recoBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+    ))}
+  </div>
+}
 
 export default function Home() {
   return (
-    <div className={styles.page}>인덱스 페이지</div>
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        <RecoBooks />
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+          <AllBooks />
+      </section>
+    </div>
   );
 }
